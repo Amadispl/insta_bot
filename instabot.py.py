537a2a458aa4bb85295ed410efb2527a8f6854f8
JPieterwas <67434034+Amadispl@users.noreply.github.com>
2023-12-02 07:21:43 +0100

    Update instabot.py.py

--- instabot.py.py
+++ instabot.py.py
@@ -1,37 +1,52 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
 import time
-import os
+import pyodbc
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import NoSuchElementException 
+
 import random
 
-class InstaPy():
-    
+
+class InstaPy:
+    query = 'SELECT Name, Follows, Comments, Likes, Runtime FROM instabots;'
+    connectionString = f'Driver={{ODBC Driver 18 for SQL Server}};Server=tcp:eatme-server.database.windows.net,1433;Database=instabotDB;Uid=eatme-admin;Pwd=KRA@ie#13;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        instabot = {
+            'Name': row[0],
+            'Follows': row[1],
+            'Comments': row[2],
+            'Likes': row[3],
+            'Runtime': row[4]
+        }
+    update = "UPDATE instabots SET Follows=?, Comments=?, Likes=?, Runtime=? WHERE Name=?"
+    conn.close()
     def __init__(self):
-        self.chrome_options = Options()     
+        self.chrome_options = Options()
         self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument("--disable-dev-shm-usage")
         self.service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=self.service, options=self.chrome_options)    
-    ### ---> login
-    ###Function is responsible for open browser and login with username and password.
+        self.driver = webdriver.Chrome(service=self.service, options=self.chrome_options)
+
+        #Funkcja odpowiedzialna za otworzenie instagrama i zalogowanie.
     def login(self):
-        self.login = "blazesienna" ### <--- Tu wprowadź login
-        self.password = "FKllWXA4CQ3vrBX!" ### <--- Tu wprowadź hasło
         self.driver.get("https://www.instagram.com/accounts/login/")
+        self.login = "blazesienna"  ### <--- Tu wprowadź login
+        self.password = "FKllWXA4CQ3vrBX!"  ### <--- Tu wprowadź hasło
         time.sleep(5)
-        #---> click to accept cookies.    
+        # ---> Zaakceptowanie Cookies.
         self.acceptbutton = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]')
+        if self.acceptbutton is None:
+            self.acceptbutton = self.driver.find_element(By.XPATH,'/html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]')
         time.sleep(2)
         self.acceptbutton.click()
         time.sleep(2)
-        ##---> end    
+        #Wypełnienie i wysłanie formularza logowania.
         self.emailForm = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[1]/div/label/input')
         self.emailForm.click()
         self.emailForm.send_keys(self.login)
@@ -44,192 +59,242 @@ class InstaPy():
         time.sleep(1)
         self.loginButton.click()
         time.sleep(15)
+        self.instabot['Runtime']+=27
 
+    ### Funkcja odpowiedzialna za wybór hashtagu i wyszukanie go na instagramie.
+    def add_hashtags_and_search(self):
 
-    ### ---> add_hashtags_and_search
-    ### Function is responsible for open and run browser with random hashtag form hashtags[]
-    def add_hashtags_and_search(self):   
-        
-        self.hashtags = ["love","instagood","photooftheday",
-    "fashion",
-    "beautiful",
-    "happy",
-    "cute",
-    "tbt",
-    "like4like",
-    "followme",
-    "picoftheday",
-    "Follow",
-    "me",
-    "selfie",
-    "summer",
-    "art",
-    "instadaily",
-    "friends",
-    "repost",
-    "nature",
-    "girl",
-    "fun",
-    "style",
-    "smile",
-    "food",
-    "family",
-    "travel",
-    "fitness",
-    "igers",
-    "tagsforlikes",
-    "follow4follow",
-    "nofilter",
-    "life",
-    "beauty",
-    "amazing",
-    "instagram",
-    "photography",
-    "photo",
-    "vscocam",
-    "sun",]
+        self.hashtags = [
+            "me",
+            "selfie",
+            "summer",
+            "art",
+            "instadaily",
+            "friends",
+            "repost",
+            "nature",
+            "girl",
+            "fun",
+            "style",
+            "smile",
+            "food",
+            "family",
+            "travel",
+            "fitness",
+            "igers",
+            "beauty",
+            "amazing",
+            "sun", ]
         self.liczba = len(self.hashtags)
-        self.hashtags_rand = random.randint(0, self.liczba -1)
+        self.hashtags_rand = random.randint(0, self.liczba - 1)
         time.sleep(4)
-        self.driver.get('https://www.instagram.com/explore/tags/' + self.hashtags[self.hashtags_rand] + '/' )
+        self.driver.get('https://www.instagram.com/explore/tags/' + self.hashtags[self.hashtags_rand] + '/')
         time.sleep(3)
-        self.rand = random.randint(1000,3000)
+        self.rand = random.randint(1000, 3000)
         self.rand = str(self.rand)
         self.driver.refresh()
         time.sleep(5)
         self.driver.execute_script("window.scrollTo(10," + self.rand + ")")
-        print('current #', self.hashtags[self.hashtags_rand])
-        self.hashtags.pop(self.hashtags_rand)
+        print('Wybrany #:', self.hashtags[self.hashtags_rand])
         time.sleep(3)
-    
-    ### ---> open_photo
-    ### Function is responsible for open random picture on website.     
-    def open_photo(self): 
-        
-        self.left = random.randint(1,2)
-        self.left = str(self.left)
-        self.downIndex = random.randint(1,2)
+        self.instabot['Runtime'] +=15
+    ###Funkcja odpowiedzialna za otwracie losowego zdjęcia na stronie.
+    def open_photo(self):
+
+        self.downIndex = random.randint(1, 2)
         self.downIndex = str(self.downIndex)
-        self.photo = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/article/div/div/div/div['+self.downIndex+']/div['+self.left+']/a')
-        self.photo.click()                    
+        self.leftIndex = random.randint(2, 3)
+        self.leftIndex = str(self.leftIndex)
+        try:
+            self.photo = self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/article/div/div/div/div[' + self.downIndex + ']/div[' + self.leftIndex + ']/a')
+        except Exception:
+            self.driver.refresh()
+        self.photo.click()
         time.sleep(2)
-        
-        
-    ### ---> skip_photo
-    ### Function is responsible for random skipp to next photo.    
+        self.instabot['Runtime'] += 2
+
+    ###Funkcja odpowiedzialna za losowe pominięcie zdjęć.   
     def skip_photo(self):
         time.sleep(2)
-        self.skip_photo_button = self.driver.find_elements(By.CLASS_NAME,'_abl-')
-        self.logic_skip_photo = random.randint(1,3)
+        self.skip_photo_button = self.driver.find_elements(By.CLASS_NAME, '_abl-')
+        self.logic_skip_photo = random.randint(1, 3)
         self.i = 0
         while self.i < self.logic_skip_photo:
             time.sleep(1.5)
-            self.skip_photo_button = self.driver.find_elements(By.CLASS_NAME,'_abl-')
+            self.skip_photo_button = self.driver.find_elements(By.CLASS_NAME, '_abl-')
             time.sleep(1)
             self.skip_photo_button[1].click()
             self.i = self.i + 1
-            print('skip {} photo, {}'.format(self.logic_skip_photo, self.i))    
-            
-     
-    ### ---> like_photo_or_get_info
-    ### Function is responsible for get user name and like photo.   
-    def like_photo_or_get_info(self):
-        
+        if self.logic_skip_photo == 1:
+            print("Pominięto: " + str(self.logic_skip_photo) + " zdjęcie")
+        else:
+            print("Pominięto: " + str(self.logic_skip_photo) + " zdjęcia")
+        self.instabot['Runtime'] += 4
+    ### Funkcja odpowiedzialnie za zostawienie like i napisania losowego komentarza(ok 10% szans).
+    def like_photo_and_comment(self):
         time.sleep(0.5)
-        self.like_button = self.driver.find_elements(By.CLASS_NAME,'xcdnw81')
+        self.like_button = self.driver.find_elements(By.CLASS_NAME, 'xcdnw81')
         time.sleep(0.5)
         self.like_button[2].click()
-        self.sendif=random.randint(1,100)
-        self.rand = random.randint(1,5)
-        self.x=0
-        if self.sendif>90:
-            self.comm = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[1]/div')
-            self.comm.click()
-            time.sleep(2)
-            while self.x<self.rand:
-                self.emote = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[4]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div[7]/div')      
-                self.emote.click()
-                time.sleep(0.5)
-                self.x=self.x+1
-            self.sent=self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]/div')
-            self.sent.click()
-            print("Zdjęcie polaikowane i komentarz")
+        self.commentif = random.randint(1, 100)
+        self.followif = random.randint(1, 100)
+        self.rand = random.randint(1, 5)
+        self.instabot['Runtime'] += 1
+        self.x = 0
+        if self.commentif > 80 and self.followif > 50:
+            self.instabot['Likes'] += 1
+            try:
+                self.left_comment()
+                print("Zdjęcie polaikowane, komentarz i follow")
+                self.instabot['Comments'] += 1
+
+                try:
+                    self.left_follow()
+                    print("Zdjęcie polaikowane, komentarz i follow")
+                    self.instabot['Follows'] += 1
+                except:
+                    print("Zdjęcie polaikowane i komentarz, nie można zostawić followa")
+                    self.instabot['Likes'] += 1
+                    self.instabot['Comments'] += 1
+            except:
+                try:
+                    self.left_follow()
+                    self.instabot['Follows'] += 1
+                    print("Zdjęcie polaikowane, komentarz i follow")
+                except:
+                    print("Zdjęcie polaikowane, nie można zostawić followa i komentarza")
+                    self.instabot['Likes'] += 1
+                print("Zdjęcie polaikowane i follow, nie można zostawić komentarza")
+        elif self.commentif > 80:
+            self.instabot['Likes'] += 1
+            try:
+                self.left_comment()
+                self.instabot['Comments'] += 1
+                print("Zdjęcie polaikowane i komentarz")
+            except:
+                print("Zdjęcie polaikowane, nie można zostawić komentarza")
+                self.instabot['Likes'] += 1
+        elif self.followif > 50:
+            try:
+                self.left_follow()
+                self.instabot['Follows'] += 1
+                self.instabot['Likes'] += 1
+                print("Zdjęcie polaikowane i follow")
+            except:
+                print("Zdjęcie polaikowane, nie można zostawić followa")
+                self.instabot['Likes'] += 1
+
         else:
             print("Tylko like")
-        
-        
-    ### ---> next_photo
-    ### Function is responsible for click to next photo.  
+            self.instabot['Likes'] += 1
+
+    ###Funkcja odpowiedzialna za zostawienie komentarza.      
+    def left_comment(self):
+        self.comm = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[1]/div')
+        self.comm.click()
+        time.sleep(2)
+        self.instabot['Runtime'] += 2
+        while self.x < self.rand:
+            self.emote = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[4]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div[7]/div')
+            self.emote.click()
+            time.sleep(0.5)
+            self.x = self.x + 1
+            self.instabot['Runtime'] += 1
+        self.sent = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]/div')
+        self.sent.click()
+
+    ###Funkcja odpowiedzialna za zostawienie followa.
+    def left_follow(self):
+        self.follow = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]/button')
+        self.follow.click()
+        time.sleep(2)
+        self.instabot['Runtime'] += 2
+
+    ###Funkcja odpowiedzialna za kliknięcie "Dalej".
     def next_photo(self):
         time.sleep(2)
         self.next_photo_button = self.driver.find_element(By.XPATH,'/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]/button')
         time.sleep(2)
         self.next_photo_button.click()
-     
-        
-    ### ---> relax
-    
-    ### Function is responsible for pause.   
+        self.instabot['Runtime'] += 4
+
+    ###Funkcja odpowiedzialna za przerwę w działaniu bota. 
     def relax(self):
-        
-        self.relax_time = random.randint(600,900)
-        print('InstaBotPy has stopped at this moment. The system will boot up in ', self.relax_time, ' seconds')
-        time.sleep(self.relax_time)
-        
-    ### ---> change hash
-    ### Function is responsible change hash.     
+        self.relax_time = random.randint(60, 90)
+        self.y = 0
+        self.count = self.relax_time
+        self.instabot['Runtime'] += self.count
+        print("")
+        for self.y in range(self.relax_time):
+            self.relax_time = self.relax_time - 1
+            print('InstaBot zrobił sobie chwilę przerwy, wróci do pracy za: ' + str(self.relax_time) + 's', end="\r")
+            time.sleep(1)
+        print("InstaBot zrobił sobie " + str(self.count) + "s przerwy,ale wrócił już do pracy :)", end="\r")
+        print("")
+
+    ### Funkcja odpowiedzialna za zmiane hashtagu.
     def change_hash(self):
-        
-        self.hashtags_rand = random.randint(0, self.liczba -1)
+
+        self.hashtags_rand = random.randint(0, self.liczba - 1)
         time.sleep(2)
-        self.driver.get('https://www.instagram.com/explore/tags/' + self.hashtags[self.hashtags_rand] + '/' )
-        print('# changed to # ' + self.hashtags[self.hashtags_rand])
-        self.hashtags.pop(self.hashtags_rand)
+        self.driver.get('https://www.instagram.com/explore/tags/' + self.hashtags[self.hashtags_rand] + '/')
+        print('# zmieniony na: ' + self.hashtags[self.hashtags_rand])
         time.sleep(5)
-    
+        self.instabot['Runtime'] += 7
+
     def random_like(self):
-        
-        self.amount = random.randint(20,22) # ammount like for # sesion.
-        print('quantity like at this session - ', self.amount)
-        
-        
-    ### ---> auto_like
-    ### Function is responsible for auto like photo. 
+
+        self.amount = random.randint(2, 5)  # Liczba polubień na jedną sesję.
+        print('Ilość like na sesje: ', self.amount)
+
+    ###Funkcja odpowiedzialna za polubienie zdjęć.
     def auto_like(self):
-        
+
         self.random_like()
         p_liked = 0
         while True:
-            
-          if p_liked == self.amount: # liczba przejsc petli:
-              self.change_hash() 
-              p_liked = 0
-              self.relax()
-              self.driver.refresh()
-              time.sleep(7)
-              try:
-                  self.open_photo()
-              except Exception:     
-                  self.open_photo()
-              time.sleep(4)
-              self.skip_photo()
-          else:
-            time.sleep(5)
-            try:
-                self.like_photo_or_get_info()
-            except Exception:     
-                print("bład")      
-            self.relax_after_like = random.randint(2,14)
-            time.sleep(self.relax_after_like)
-            try:
-                self.next_photo()
-            except Exception:
+            if p_liked == self.amount:  # liczba przejść petli:
                 self.change_hash()
-            p_liked = p_liked + 1
+                p_liked = 0
+                self.relax()
+                self.driver.refresh()
+                time.sleep(4)
+                self.instabot['Runtime'] += 4
+                try:
+                    self.open_photo()
+                except Exception:
+                    self.open_photo()
+                time.sleep(4)
+                self.instabot['Runtime'] += 4
+                self.skip_photo()
+            else:
+                time.sleep(5)
+                self.instabot['Runtime'] += 5
+                try:
+                    self.like_photo_and_comment()
+                except Exception:
+                    print("Coś poszło nie tak")
+                self.relax_after_like = random.randint(1, 4)
+                time.sleep(self.relax_after_like)
+                self.instabot['Runtime'] += self.relax_after_like
+                try:
+                    self.next_photo()
+                except Exception:
+                    self.auto_like()
+                    self.instabot['Runtime'] += 4
+                p_liked = p_liked + 1
+                self.conn = pyodbc.connect(self.connectionString)
+                self.cursor = self.conn.cursor()
+                print(self.instabot)
+                self.cursor.execute(self.update,self.instabot['Follows'], self.instabot['Comments'], self.instabot['Likes'], self.instabot['Runtime'],self.instabot['Name'])
+                self.conn.commit()
+                self.cursor.close()
+                self.conn.close()
+
 
 bot = InstaPy()
 bot.login()
-bot.add_hashtags_and_search()     
-bot.open_photo()   
+bot.add_hashtags_and_search()
+bot.open_photo()
 bot.skip_photo()
 bot.auto_like()
